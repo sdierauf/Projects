38bd4b9785df2a8a8a9a7f54ab4d4d28bc9645b9
Stefan <sdierauf@gmail.com>
2013-12-30 09:24:14 -0800

    recipes.js done as much as I want it to be

--- Classes/Recipes.js
+++ Classes/Recipes.js
@@ -3,8 +3,6 @@ function Ingredient(name, flavor) {
   this.flavor = flavor;
 }
 
-
-
 function Recipe(name) {
   this.name = name;
   //this.category = "";
@@ -42,16 +40,85 @@ function CookBook(title) {
   this.recipes = [];
   this.addRecipe = function(recipe) {
     var category = this.determine(recipe);
+    console.log(category);
     if (this.recipes.indexOf(recipe) == -1) {
       this.recipes.push({recipe:recipe, category:category});
     }
   }
+
+  this.printSection = function(section) {
+    section = section.toLowerCase();
+    console.log("Printing all recipes in the "  + section + " section");
+    this.recipes.forEach(function(value) {
+      if (value.category == section) {
+        value.recipe.printPretty();
+      }
+    });
+  }
+  
+  this.printAll = function() {
+    this.recipes.forEach( function(value) {
+      value.recipe.printPretty();
+    });
+  }
+
+  this.determine = function(recipe) {
+    var flavorMap = {sweet:0, savory:0, bitter:0, sour:0};
+    recipe.ingredients.forEach( function(value) {
+      switch(value.ingredient.flavor) {
+        case "sweet":
+          flavorMap.sweet += value.amount;
+          break;
+        case "savory":
+          flavorMap.savory += value.amount;
+          break;
+        case "bitter":
+          console.log("bitter!");
+          flavorMap.bitter += value.amount;
+          console.log(flavorMap.bitter);
+          break;
+        case "sour":
+          flavorMap.sour += value.amount;
+          break;
+        default:
+          console.log("badly formatted ingredient");
+      }
+    });
+    var flavor = {flavor:"sweet", amount:flavorMap.sweet};
+    for (var propt in flavorMap) {
+      if (flavorMap[propt] > flavor.amount) {
+        flavor.flavor = propt + "";
+        flavor.amount = flavorMap[propt];
+      }
+    }
+    console.log(flavor.flavor);
+    console.log(flavor);
+    //get most popular flavor
+    //set category
+    switch(flavor.flavor) {
+      case "sweet":
+        return "breakfast";
+        break;
+      case "savory":
+        return "dinner";
+        break;
+      case "bitter":
+        return "snack";
+        break;
+      case "sour":
+        return "lunch";
+        break;
+      default:
+        return "snack";
+    }
+  }
 }
 
-var p = new Ingredient("poop", "sour");
+var p = new Ingredient("poop", "bitter");
 var rec = new Recipe("poopy poop");
 rec.addIngredient(p, 5)
 rec.addIngredient(p, 10);
-
+var book = new CookBook("Cooking with Poop");
+book.addRecipe(rec);
 console.log(rec.ingredients);
 rec.printPretty();
